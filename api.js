d9ef998c76645eaece6b3f5c3b38e5b7b3dfed73
Jesus Arevalo <isa90201@gmail.com>
2018-10-23 14:17:59 -0700

    Updated API

--- api.js
+++ api.js
@@ -1,2 +1,4 @@
-exports.DbConnection = require('./dbconnection.js');
-exports.Results = require('./results.js');
+exports.Connection = require('./connection.js').Connection;
+exports.Pool = require('./pool.js').Pool;
+exports.RowResult = require('./results.js').RowResult;
+exports.InfoResult = require('./results.js').InfoResult;
